07c13585f8222a9fd040a0df1259b634a9e683b7
Qize <louisliu1206@yahoo.com>
2020-07-04 03:42:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function deleteFromObjectByKey(object, key) {
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
-  delete newObject.[key]
+  delete newObject[key]
   return newObject;
 }
